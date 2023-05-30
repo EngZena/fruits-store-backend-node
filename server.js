b1d20763ea03109dd1459f4d7677ee016c691885
engZena <zena_al_ja95@yahoo.com>
2023-05-30 22:42:38 +0300

    log uncaughtException and unhandledRejection

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const dotenv = require('dotenv');
 const C = require('./utils/log');
 
+/**
+ * uncaughtException means no code was looking for that execption
+ */
 process.on('uncaughtException', (error) => {
   C(`uncaughtException 💥💥 ${error.name}: ${error.message}`);
 });
@@ -14,6 +17,10 @@ const server = app.listen(port, () => {
   C(`App running on port ${port}...`);
 });
 
+/**
+ * unhandledrejection event is sent to the global scope of a script when a JavaScript Promise
+ * that has no rejection handler is rejected
+ */
 process.on('unhandledRejection', (error) => {
   C(`unhandledRejection 💥💥 ${error.name}: ${error.message}`);
   server.close(() => {
