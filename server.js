385fefa7540552ed14dcbcb35597bccc623cf22e
engZena <zena_al_ja95@yahoo.com>
2023-05-30 22:16:24 +0300

    log uncaughtException and unhandledRejection

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const dotenv = require('dotenv');
+const C = require('./utils/log');
 
+process.on('uncaughtException', (error) => {
+  C(`uncaughtException 💥💥 ${error.name}: ${error.message}`);
+});
 dotenv.config({ path: `${__dirname}/config.env` });
 
 const app = require('./app');
-const C = require('./utils/log');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   C(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (error) => {
+  C(`unhandledRejection 💥💥 ${error.name}: ${error.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
