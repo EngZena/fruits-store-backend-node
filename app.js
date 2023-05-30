a7ce09725390704b28d15b950eabe0f8048051fd
engZena <zena_al_ja95@yahoo.com>
2023-05-30 22:22:23 +0300

    install helmet library and use it

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", 'https:', 'http:', 'data:', 'ws:'],
+      baseUri: ["'self'"],
+      fontSrc: ["'self'", 'https:', 'http:', 'data:'],
+      scriptSrc: ["'self'", 'https:', 'http:', 'blob:'],
+      styleSrc: ["'self'", "'unsafe-inline'", 'https:', 'http:'],
+    },
+  })
+);
 
 module.exports = app;
