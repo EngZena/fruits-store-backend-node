ae28205bcee443973b88f4e04c355a70c3ec3e10
engZena <zena_al_ja95@yahoo.com>
2023-06-04 00:59:17 +0300

    save password hashed

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,7 +34,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
+    passwordConfirm: req.body.password,
     role: req.body.role,
   });
   const url = `${req.protocol}://${req.get('host')}/me`;
