4d8e314cbe9fb020d56bc8d04dc64b554315c5fc
engZena <zena_al_ja95@yahoo.com>
2023-07-28 17:42:30 +0300

    use es6 syntax: 1.(import/export) 2.arrow functions

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,8 +1,8 @@
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-const APIFeatures = require('../utils/apiFeatures');
+import APIFeatures from '../utils/apiFeatures';
+import AppError from '../utils/appError';
+import catchAsync from '../utils/catchAsync';
 
-exports.createOne = (Model) =>
+export const createOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const newDocument = await Model.create(req.body);
     res.status(201).json({
@@ -13,7 +13,7 @@ exports.createOne = (Model) =>
     });
   });
 
-exports.getOne = (Model, popOptions) =>
+export const getOne = (Model, popOptions) =>
   catchAsync(async (req, res, next) => {
     let query = Model.findById(req.params.id);
     if (popOptions) query = query.populate(popOptions);
@@ -29,7 +29,7 @@ exports.getOne = (Model, popOptions) =>
     });
   });
 
-exports.getAll = (Model) =>
+export const getAll = (Model) =>
   catchAsync(async (req, res, next) => {
     let filter = {};
     if (req.params.fruitid) filter = { fruit: req.params.fruitid };
@@ -49,7 +49,7 @@ exports.getAll = (Model) =>
     });
   });
 
-exports.updateOne = (Model) =>
+export const updateOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const document = await Model.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -66,7 +66,7 @@ exports.updateOne = (Model) =>
     });
   });
 
-exports.deleteOne = (Model) =>
+export const deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
     if (!doc) {
