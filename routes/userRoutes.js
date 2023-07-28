92229cde7d132048316471f269bd482f849d51c2
engZena <zena_al_ja95@yahoo.com>
2023-07-28 22:12:51 +0300

    update resetPassword route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,6 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 router.post('/updateMyPassword', authController.forgotPassword);
-router.post('/resetPassword/:token', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 export default router;
